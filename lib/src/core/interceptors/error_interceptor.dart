712a304adcd4a50dcc851325ace2c2a9c1a1feea
Anton <anton@2muchcoffee.com>
2023-06-07 19:38:06 +0300

    feat(core:interceptors:error): handle expired token error

--- lib/src/core/interceptors/error_interceptor.dart
+++ lib/src/core/interceptors/error_interceptor.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 
+import 'package:go_router/go_router.dart';
 import 'package:http_interceptor/http_interceptor.dart';
-import 'package:notesy_flutter/src/repos/app_repository.dart';
+
+import '../../bloc/blocs.dart';
+import '../../repos/repos.dart';
 
 class ErrorInterceptor implements InterceptorContract {
+  final _appRepository = AppRepository();
+
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
     return data;
@@ -13,7 +18,16 @@ class ErrorInterceptor implements InterceptorContract {
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
     if (data.statusCode >= 400) {
       final decodedResponse = json.decode(data.body!);
-      AppRepository().showToastr(message: decodedResponse['message']);
+      final String message = decodedResponse['message'];
+
+      // TODO: refactor token expiration condition when api is updated to return either specific status or specific bool property
+      if (message.contains('Firebase ID token has expired')) {
+        AuthCubit().logout();
+        GoRouter.of(_appRepository.navigatorKey.currentContext!).go('/login');
+        _appRepository.showToastr(message: 'Session expired. Please login');
+      } else {
+        _appRepository.showToastr(message: message);
+      }
     }
     return data;
   }
